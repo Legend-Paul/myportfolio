ee674c8c9ac52229883291a3fa0d515aeda9924e
Legend Paul <qamunyamaina950@gmail.com>
2025-05-20 08:53:52 +0300

    create footer element

--- script.js
+++ script.js
@@ -2,32 +2,47 @@ let navBtn = document.querySelectorAll(".nav-btn");
 let body = document.querySelector(".body");
 let sections = document.querySelectorAll(".section");
 let nav = document.querySelector(".nav-bar");
-
-navBtn.forEach(btn=>{
-    
-    btn.addEventListener("click", function(){
+let footer = document.querySelector("footer");
+let prevScroll = 0;
+navBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
         let currentBtn = document.querySelector(".active");
         if (currentBtn) {
             currentBtn.classList.remove("active");
         }
-        console.log(this);
+
         this.classList.add("active");
-        console.log(btn);
     });
 });
 
-window.onscroll = ()=>{
-    sections.forEach(section=>{
+window.onscroll = () => {
+    sections.forEach((section) => {
         let scrollingHeight = window.scrollY;
         let sectionTop = section.offsetTop;
         let height = section.offsetHeight;
         let id = section.getAttribute("id");
-
-        if (scrollingHeight >= sectionTop && scrollingHeight < sectionTop + height){
-            navBtn.forEach(Btn=>{
+        positionFooter(scrollingHeight);
+        if (
+            scrollingHeight >= sectionTop &&
+            scrollingHeight < sectionTop + height
+        ) {
+            navBtn.forEach((Btn) => {
                 Btn.classList.remove("active");
-                document.querySelector(`nav a div[data-id="${id}"]`).classList.add("active");
+                document
+                    .querySelector(`nav a div[data-id="${id}"]`)
+                    .classList.add("active");
             });
         }
     });
+};
+
+function positionFooter(scrollingHeight) {
+    if (prevScroll !== scrollingHeight) {
+        if (prevScroll <= scrollingHeight) {
+            footer.style.position = "relative";
+        } else {
+            footer.style.position = "fixed";
+        }
+        prevScroll = scrollingHeight;
+    }
 }
