822c0e67a2c40590d856df1b9e3a02231a5aa94f
Legend-Paul <qamunyamaina950@gmail.com>
2025-12-21 09:21:00 +0300

    Remove file clod project and add bloger project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,23 @@
-
 export const projects = [
-    {
-    title: "File Cloud",
-    description: `FileCloud it allows users to upload, store, view, and share files directly through the browser.
-The goal of this project is to provide a lightweight for managing files locally or across connected users.`,
-    image: "images/projects/filecloud.png",
-    technologies: ["EXPRESS", "EJS", "CSS3"],
-    codeLink: "https://github.com/Legend-Paul/FileCloud",
-    liveLink: "https://legendpaul-filecloud.onrender.com/"
+  {
+    title: "Blog App",
+    description: `A full-stack blogging application that allows users to create, edit, delete, and view blog posts. 
+It features user authentication, responsive design, and a rich text editor for composing posts.`,
+    image: "images/projects/bloger.png",
+    technologies: ["POSTGRESQL", "EXPRESS", "REACT", "NODEJS", "CSS3"],
+    codeLink: "https://github.com/Legend-Paul/blog.git",
+    liveLink: "https://legendbloger.onrender.com",
   },
+  //     {
+  //     title: "File Cloud",
+  //     description: `FileCloud it allows users to upload, store, view, and share files directly through the browser.
+  // The goal of this project is to provide a lightweight for managing files locally or across connected users.`,
+  //     image: "images/projects/filecloud.png",
+  //     technologies: ["EXPRESS", "EJS", "CSS3"],
+  //     codeLink: "https://github.com/Legend-Paul/FileCloud",
+  //     liveLink: "https://legendpaul-filecloud.onrender.com/"
+  //   },
 
-  
   {
     title: "E Mall",
     description: `E-Mall, a responsive e-commerce. Product browsing and details. Add, update, and remove items from the cart. Cart persistence using LocalStorage. Smooth navigation with React Router
@@ -18,7 +25,7 @@ The goal of this project is to provide a lightweight for managing files locally 
     image: "images/projects/emall.png",
     technologies: ["REACT", "CSS3", "JAVASCRIPT"],
     codeLink: "https://github.com/Legend-Paul/E-MALL",
-    liveLink: "https://legendpaulemall.netlify.app/"
+    liveLink: "https://legendpaulemall.netlify.app/",
   },
 
   {
@@ -28,19 +35,16 @@ This project helped sharpen my skills in React, state management, and UI/UX desi
     image: "images/projects/cv.png",
     technologies: ["REACT", "CSS3", "JAVASCRIPT"],
     codeLink: "https://github.com/Legend-Paul/CV-Application",
-    liveLink: "https://legendpaulcvapplication.netlify.app/"
+    liveLink: "https://legendpaulcvapplication.netlify.app/",
   },
-  
+
   {
-    
     title: "INVENTRACK",
     description: `This is a simple Inventory Management System built with modern web tools.
 It allows you to keep track of items, categories, and all the details you need for managing stock.`,
     image: "images/projects/inventrack.png",
     technologies: ["EXPRES", "EJS", "CSS3"],
     codeLink: "https://github.com/Legend-Paul/INVENTORY",
-    liveLink: "https://legendpaul-inventory.onrender.com/"
+    liveLink: "https://legendpaul-inventory.onrender.com/",
   },
-  
-  
 ];
